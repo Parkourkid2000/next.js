ab99e657e0e4eaeb70d0014378a58e3693b87a2b
Parkourkid2000 <deleona144@gmail.com>
2025-02-15 21:12:57 -0500

    first commit

---
+++ pages/posts/sports/trending/jack.js
@@ -0,0 +1,5 @@
+export default function Jack() {
+return (
+    <h1>this is jacks route</h1>
+)
+}
---
+++ pages/users/index.js
@@ -0,0 +1,11 @@
+export default function Users(){
+    return (
+<>
+    
+            <h1>
+                this is the Users page
+            </h1>
+</>
+
+    )
+}
